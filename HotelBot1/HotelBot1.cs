09009133a736fe55c38f42e670a0d0564e129bda
Juliet Moreiro Bockhop <julietsvq@gmail.com>
2019-06-07 00:39:08 +0200

    Adding code to process use input using LUIS

--- HotelBot1/HotelBot1.cs
+++ HotelBot1/HotelBot1.cs
@@ -6,20 +6,78 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using System.Linq;
+using Microsoft.Bot.Builder.AI.Luis;
 
 namespace HotelBot1
 {
     public class EmptyBot : ActivityHandler
     {
+        public const string RestaurantIntent = "BookRestaurant";
+        public const string SpaIntent = "BookSPA";
+        public const string TouristIntent = "TouristInfo";
+        private LuisRecognizer _recognizer { get; } = null;
+
+        public EmptyBot(LuisRecognizer recognizer) : base()
+        {
+            _recognizer = recognizer ?? throw new System.ArgumentNullException(nameof(recognizer));
+        }
+
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text($"Hello world!"), cancellationToken);
+                    await turnContext.SendActivityAsync(MessageFactory.Text("Hello, how can I help you today?"));
                 }
             }
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var recognizerResult = await _recognizer.RecognizeAsync(turnContext, cancellationToken);
+            var (intent, score) = recognizerResult.GetTopScoringIntent();
+            var entities = recognizerResult.Entities;
+
+            string message = "";
+            var people = entities["number"]?.FirstOrDefault()?.ToString() ?? "1";
+            var date = entities["datetime"]?.FirstOrDefault()["timex"]?.FirstOrDefault()?.ToString() ?? "today";
+            var topic = entities["TouristAttraction"]?.FirstOrDefault()?.FirstOrDefault()?.ToString() ?? "the city";
+
+            switch (intent)
+            {
+                case RestaurantIntent:
+                    message = $"We will book a table for {people} people for {date}.";
+                    break;
+                case SpaIntent:
+                    message = $"We will book an appointment for the Spa for {people} people for {date}.";
+                    break;
+                case TouristIntent:
+                    message = $"We will provide you with information about {topic}";
+                    break;
+                default:
+                    message = "Sorry, I did not understand you, how may I help you?";
+                    break;
+            }
+
+            await turnContext.SendActivityAsync(message);
+        }
+
+        private static async Task SendSuggestedActionsAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            var reply = turnContext.Activity.CreateReply("Hello, what can I help you with today?");
+            reply.SuggestedActions = new SuggestedActions()
+            {
+                Actions = new List<CardAction>()
+                {
+                    new CardAction() { Title = "SPA", Type = ActionTypes.ImBack, Value = "SPA" },
+                    new CardAction() { Title = "Restaurant", Type = ActionTypes.ImBack, Value = "Restaurant" },
+                    new CardAction() { Title = "Tourist information", Type = ActionTypes.ImBack, Value = "Tourist information" }
+                }
+            };
+            
+            await turnContext.SendActivityAsync(reply, cancellationToken);
         }
     }
 }
--- HotelBot1/Startup.cs
+++ HotelBot1/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Bot.Builder.AI.Luis;
 
 namespace HotelBot1
 {
@@ -36,6 +37,13 @@ namespace HotelBot1
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, EmptyBot>();
+
+            services.AddSingleton(sp =>
+            {
+                var luisApp = new LuisApplication(Configuration["LuisAppId"], Configuration["LuisAPIKey"], Configuration["LuisAPIHostName"]);
+                var recognizer = new LuisRecognizer(luisApp);
+                return recognizer;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
